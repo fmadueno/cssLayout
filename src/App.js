c8e4c6045b6c06627a22ee3dd9584e7b138185e7
fmadueno <fmaduenoma@gmail.com>
2020-11-25 19:05:50 +0100

    Primera versión.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './maquetacion.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div className="navbar">
+                <a href="http://" className="nav-el">CSS Layout</a>
+                <a href="http://" className="nav-el">CSS Layout</a>
+                <a href="http://" className="nav-el">CSS</a>
+                <a href="http://" className="nav-el">CSS Layout</a>
+                <a href="http://" className="nav-el">CSS Layout</a>
+            </div>
+            <div className ="content">
+                <div className="card">
+                    <div className="card-title">
+                        Card1
+                    </div>
+                    <div className="card-body">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-title">
+                        Card2
+                    </div>
+                    <div className="card-body">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-title">
+                        Card3
+                    </div>
+                    <div className="card-body">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    </div>
+                </div>
+            </div>
+
+            <footer className="footer">footer</footer>
+        </div>
+    );
 }
 
 export default App;
